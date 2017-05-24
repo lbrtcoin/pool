6f55786ffb958ee810f1a8b54988d299da3cc8ba
lbrtcoin@gmail.com <lbrtcoin@gmail.com>
2017-05-24 18:06:28 +0000

    exposed port

--- docker/start.sh
+++ docker/start.sh
@@ -4,5 +4,5 @@ if [ "$(docker ps -a | grep [u]nomp)" != "" ];
 then
   docker start pool 
 else
-  docker run -d --name pool -v $(pwd)/conf/config.json:/root/unomp/config.json -v $(pwd)/conf/litecoin.json:/root/unomp/pool_configs/litecoin.json pool
+  docker run -d --name pool -p 80:80 -v $(pwd)/conf/config.json:/root/unomp/config.json -v $(pwd)/conf/litecoin.json:/root/unomp/pool_configs/litecoin.json pool
 fi
